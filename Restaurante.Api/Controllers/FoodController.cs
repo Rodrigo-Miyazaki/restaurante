64f5294ee952f72c3d2ac011408b63c30379af43
Rodrigo Miyazaki <rodrigo.miyazaki@lambda3.com.br>
2022-02-08 15:14:15 -0300

    Chamando serviço de aplicação na  controller

--- Restaurante.Api/Controllers/FoodController.cs
+++ Restaurante.Api/Controllers/FoodController.cs
@@ -1,15 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Restaurante.Application.Services;
 using Restaurante.Core.Models;
 
 namespace Restaurante.Api.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/food")]
     [ApiController]
     public class FoodController : ControllerBase
     {
+        private readonly IFoodApplicationService _foodApplicationService;
+
+        public FoodController(IFoodApplicationService foodApplicationService)
+        {
+            _foodApplicationService = foodApplicationService;
+        }
+
         [HttpPost]
         public IActionResult Add(Food food)
         {
+            _foodApplicationService.Add(food);
             return Ok();
         }
     }
