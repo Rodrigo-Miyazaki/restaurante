e33fe7e4eb1084264f606e086645c1b2b8f34728
Rodrigo Miyazaki <rodrigo.mzk@gmail.com>
2022-02-08 08:30:04 -0300

    Criando tabela Foods

---
+++ Restaurante.Api/EntityFramework/Mappings/FoodMap.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Restaurante.Api.Models;
+
+namespace Restaurante.Api.EntityFramework.Mappings
+{
+    public class FoodMap : IEntityTypeConfiguration<Food>
+    {
+        public void Configure(EntityTypeBuilder<Food> builder)
+        {
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.Id)
+                .HasColumnName("id")
+                .ValueGeneratedOnAdd();
+
+            builder.Property(c => c.Name)
+                .HasColumnName("name")
+                .IsRequired();
+
+            builder.ToTable("Foods");
+        }
+    }
+}
