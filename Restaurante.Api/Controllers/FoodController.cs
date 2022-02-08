9f159dc0325a4cb11609165e2f7649e1377554fa
Rodrigo Miyazaki <rodrigo.miyazaki@lambda3.com.br>
2022-02-08 12:55:39 -0300

    Criando endpoint de adicionar comida

---
+++ Restaurante.Api/Controllers/FoodController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Restaurante.Core.Models;
+
+namespace Restaurante.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FoodController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Add(Food food)
+        {
+            return Ok();
+        }
+    }
+}
