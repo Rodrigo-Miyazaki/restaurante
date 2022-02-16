6004eea11cfb4a63736f690f0b0404ca828c6467
Rodrigo Miyazaki <rodrigo.miyazaki@lambda3.com.br>
2022-02-16 13:36:28 -0300

    Adicionando testes para adicionar comida

---
+++ Restaurante.UnitTests/Builders/FoodBuilder.cs
@@ -0,0 +1,25 @@
+﻿using AutoBogus;
+using Restaurante.Core.Models;
+
+namespace Restaurante.UnitTests.Builders
+{
+    public class FoodBuilder : AutoFaker<Food>
+    {
+        public FoodBuilder() : base("pt_BR")
+        {
+            RuleFor(x => x.Name, f => f.Name.Random.Words());
+        }
+
+        public FoodBuilder WithName(string name)
+        {
+            RuleFor(x => x.Name, name);
+            return this;
+        }
+
+        public FoodBuilder ForDb()
+        {
+            RuleFor(x => x.Id, 0);
+            return this;
+        }
+    }
+}
