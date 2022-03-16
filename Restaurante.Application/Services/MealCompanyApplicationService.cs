7724624b763af96f438c22cf5c2e69ee72fd8bfa
Rodrigo Miyazaki <rodrigo.miyazaki@lambda3.com.br>
2022-03-16 16:57:24 -0300

    Criando endpoint para adicionar referencia de refeição e empresa

---
+++ Restaurante.Application/Services/MealCompanyApplicationService.cs
@@ -0,0 +1,19 @@
+﻿using Restaurante.Application.Interfaces.Services;
+using Restaurante.Core.Models;
+using Restaurante.Infrastructure.Repositories.Intefaces;
+using System.Threading.Tasks;
+
+namespace Restaurante.Application.Services
+{
+    public class MealCompanyApplicationService : IMealCompanyApplicationService
+    {
+        private readonly IMealCompanyRepository _mealCompanyRepository;
+
+        public MealCompanyApplicationService(IMealCompanyRepository mealCompanyRepository)
+        {
+            _mealCompanyRepository = mealCompanyRepository;
+        }
+
+        public Task AddAsync(MealCompany mealCompany) => _mealCompanyRepository.AddAsync(mealCompany);
+    }
+}
---
+++ Restaurante.UnitTests/Builders/MealBuilder.cs
@@ -0,0 +1,19 @@
+﻿using AutoBogus;
+using Restaurante.Core.Models;
+using System.Collections.Generic;
+
+namespace Restaurante.UnitTests.Builders
+{
+    public class MealBuilder : AutoFaker<Meal>
+    {
+        public MealBuilder() : base("pt_BR")
+        {
+            RuleFor(x => x.Name, f => f.Name.Random.Words());
+            RuleFor(x => x.Quantity, f => f.Random.Number(0, 10));
+            RuleFor(x => x.Price, f => f.Random.Number(0, 10000));
+            RuleFor(x => x.MealType, f => f.PickRandom<MealType>());
+            RuleFor(x => x.Foods, (List<Food>)null);
+            RuleFor(x => x.Company, (Company)null);
+        }
+    }
+}
